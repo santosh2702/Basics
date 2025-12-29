b2be09d20b71d4204728b5f29b953a4c046f3c5f
Santosh Kumar <Singh.santosh2702@gmail.com>
2025-12-29 19:39:24 +0530

    Create Balanced_binart_tree.py

---
+++ Balanced_binart_tree.py
@@ -0,0 +1,17 @@
+def dfs(node):
+            if not node:
+                return 0
+
+            left = dfs(node.left)
+            if left == -1:
+                return -1
+
+            right = dfs(node.right)
+            if right == -1:
+                return -1
+
+            if abs(left - right) > 1:
+                return -1
+
+            return 1+max(left, right)
+        return dfs(root) != -1
