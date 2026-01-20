e6e4d97b104ca025206b7d68c2f20b49a62b4a4f
Santosh Kumar <Singh.santosh2702@gmail.com>
2026-01-21 02:28:43 +0530

    Create Generate_Parentheses.py

---
+++ Generate_Parentheses.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def generateParenthesis(self, n):
+      
+        res = []
+
+        def backtrack(curr, open, close):
+            if len(curr) == 2*n:
+                res.append(curr)
+                return 
+
+            if open < n:
+                backtrack(curr + "(", open + 1, close)
+            if close < open:
+                backtrack(curr + ")", open, close + 1)
+
+        backtrack("", 0, 0)
+        return res
